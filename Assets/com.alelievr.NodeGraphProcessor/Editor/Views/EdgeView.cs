08fc67d771cd4db38337307ada12d44a52dd6977
mouserices <mouse.rices@gmail.com>
2021-08-03 17:37:09 +0800

    wangwei
    EdgeView 支持节点流向

--- Assets/com.alelievr.NodeGraphProcessor/Editor/Views/EdgeView.cs
+++ Assets/com.alelievr.NodeGraphProcessor/Editor/Views/EdgeView.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine.UIElements;
 using UnityEngine;
@@ -18,6 +19,7 @@ namespace GraphProcessor
 		{
 			styleSheets.Add(Resources.Load<StyleSheet>(edgeStyle));
 			RegisterCallback<MouseDownEvent>(OnMouseDown);
+			this.AddManipulator(new FlowPoint());
 		}
 
         public override void OnPortChanged(bool isInput)
@@ -59,6 +61,66 @@ namespace GraphProcessor
 
 				owner.AddRelayNode(input as PortView, output as PortView, mousePos);
 			}
+		}
+	}
+	
+	public class FlowPoint : Manipulator
+	{
+		VisualElement point { get; set; }
+
+		protected override void RegisterCallbacksOnTarget()
+		{
+			if (target is Edge edge)
+			{
+				point = new VisualElement();
+				point.AddToClassList("flow-point");
+				point.style.left = 0;
+				point.style.top = 0;
+				target.Add(point);
+
+				target.schedule.Execute(() =>
+				{
+					UpdateCapPoint(edge, (float)(EditorApplication.timeSinceStartup % 3 / 3));
+				}).Until(() => point == null);
+			}
+		}
+
+		protected override void UnregisterCallbacksFromTarget()
+		{
+			if (point != null)
+			{
+				target.Remove(point);
+				point = null;
+			}
+		}
+
+		public void UpdateCapPoint(Edge _edge, float _t)
+		{
+			Vector3 v = Lerp(_edge.edgeControl.controlPoints, _t);
+			point.style.left = v.x;
+			point.style.top = v.y;
+		}
+
+		Vector2 Lerp(Vector2[] points, float t)
+		{
+			t = Mathf.Clamp01(t);
+			float totalLength = 0;
+			for (int i = 0; i < points.Length - 1; i++)
+			{
+				totalLength += Vector2.Distance(points[i], points[i + 1]);
+			}
+
+			float pointLength = Mathf.Lerp(0, totalLength, t);
+
+			float tempLength = 0;
+			for (int i = 0; i < points.Length - 1; i++)
+			{
+				float d = Vector2.Distance(points[i], points[i + 1]);
+				if (pointLength <= tempLength + d)
+					return Vector2.Lerp(points[i], points[i + 1], (pointLength - tempLength) / d);
+				tempLength += d;
+			}
+			return points[0];
 		}
 	}
 }
